ee4b2d93601bc0f0709a958afe9167bd1fae3666
Marcos Arribas Gómez <marcos.arribas@alumnos.upm.es>
2025-02-22 20:04:19 +0200

    Restore original mergesort.py from TUNI repository

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,25 +1,27 @@
-def debug_print(**kwargs):
+
+def debug_print(debug_msg=None, **kwargs):
+
+    if debug_msg:
+        print(debug_msg)
+
     for key, value in kwargs.items():
         print("{}: {}".format(key, value))
+
 
 def mergesort(array):
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
-    debug_print(array=array, m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
 
-    debug_print(left=left)
-    debug_print(right=right)
-
     return merge(left, right)
+
 
 def merge(left, right):
     merged = []
-    debug_print(left=left, right=right)
 
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
@@ -27,24 +29,24 @@ def merge(left, right):
         else:
             merged.append(right.pop(0))
 
-    merged += left + right
-    debug_print(merged=merged)
+    if len(left) > 0:
+        merged += left
+    else:
+        merged += right
+
     return merged
+
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
     input_list = input_str.split(",")
-
     value_list = []
     for x in input_list:
         try:
             value_list.append(int(x))
-        except ValueError:
+        except ValueError as err:
             print("Invalid input.")
             quit(1)
 
-    debug_print(input_list=input_list)
-    debug_print(value_list=value_list)
-    
     sorted_list = mergesort(value_list)
     print(sorted_list)
