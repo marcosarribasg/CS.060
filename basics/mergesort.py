8b106d6246ed30ab56864325803eb2aa0e22379e
Marcos Arribas Gómez <marcos.arribas@alumnos.upm.es>
2025-02-22 19:45:12 +0200

    Restore mergesort.py with debug print statements

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,27 +1,27 @@
-
 def debug_print(debug_msg=None, **kwargs):
-
     if debug_msg:
         print(debug_msg)
-
     for key, value in kwargs.items():
         print("{}: {}".format(key, value))
-
 
 def mergesort(array):
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
+    debug_print(array=array, m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
 
-    return merge(left, right)
+    debug_print(left=left)
+    debug_print(right=right)
 
+    return merge(left, right)
 
 def merge(left, right):
     merged = []
+    debug_print(left=left, right=right)
 
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
@@ -34,19 +34,22 @@ def merge(left, right):
     else:
         merged += right
 
+    debug_print(merged=merged)
     return merged
-
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
     input_list = input_str.split(",")
     value_list = []
+    
     for x in input_list:
         try:
             value_list.append(int(x))
         except ValueError as err:
             print("Invalid input.")
             quit(1)
+
+    debug_print(input_list=input_list, value_list=value_list)
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
